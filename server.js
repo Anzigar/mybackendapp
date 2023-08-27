ecbd173ae46d87d71cb775f82b6e6d181785c8f8
Anzigar <ansigare@gmail.com>
2023-08-27 11:59:06 +0300

    k

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import fs from "fs";
 const app = express();
 app.use(express.json());
 const httpServer = createServer(app);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const bucketName = 'journey-v1';
 
 //session 
